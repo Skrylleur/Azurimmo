996a5a2a77abed843495d78b7d6670e0dbe89607
Skrylleur <106743803+Skrylleur@users.noreply.github.com>
2025-02-10 11:48:08 +0100

    MàJ BatimentCard

--- app/src/main/java/bts/sio/azurimmo/views/batiment/BatimentCard.kt
+++ app/src/main/java/bts/sio/azurimmo/views/batiment/BatimentCard.kt
@@ -1,3 +1,4 @@
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,11 +13,12 @@ import androidx.compose.ui.unit.dp
 import bts.sio.azurimmo.model.Batiment
 
 @Composable
-fun BatimentCard(batiment: Batiment, onClick: () -> Unit) { // Notez l'annotation @Composable
+fun BatimentCard(batiment: Batiment, onClick: (Int) -> Unit) {  // Notez l'annotation @Composable
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp),
+            .padding(8.dp)
+            .clickable { onClick(batiment.id)},
         elevation = CardDefaults.elevatedCardElevation(defaultElevation = 4.dp),
         shape = RoundedCornerShape(8.dp)
     ) {
